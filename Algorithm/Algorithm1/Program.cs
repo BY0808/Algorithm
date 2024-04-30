f6985a02445c59c0a492e1598d404297f5beb390
BY0808 <byahn.13@gmail.com>
2024-04-30 15:47:05 +0900

    업뎃

---
+++ Algorithm/Algorithm1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithm1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int A = int.Parse(ss[0]);
+            int B = int.Parse(ss[1]);
+            Console.WriteLine(A + B);
+        }
+    }
+}
